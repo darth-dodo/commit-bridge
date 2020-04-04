1113712bc004ee5a800100befa0f10653153e6ca
Abhishek Juneja <abhishek.juneja145@gmail.com>
2020-04-04 17:23:47 +0200

    testing non happy status codes with the communicator and echoes

--- app/controllers/git_cloud_webhook_controller.rb
+++ app/controllers/git_cloud_webhook_controller.rb
@@ -5,7 +5,7 @@ class GitCloudWebhookController < BaseWebhookController
     service = GitCloudWebhook::WebhookEventParser.new(event)
     service.execute!
 
-    json_response(service.service_response_data)
+    json_response(service.service_response_data, :created)
 
   rescue JSON::ParserError
     head(:unprocessable_entity)
@@ -14,6 +14,12 @@ class GitCloudWebhookController < BaseWebhookController
   def echo
     data = JSON.parse(request.body.read)
     data[:time] = Time.now
-    json_response(data)
+    # render json: data, status: :internal_server_error # generic api error 500
+    # render json: data, status: :bad_request # bad request error 400
+    # render json: data, status: :unauthorized # unauthorized error 401
+    # render json: data, status: :forbidden # forbidden error 403
+    # render json: data, status: :not_found # 404
+    # render json: data, status: :unprocessable_entity # 422
+    render(json: data, status: :ok) # OK 200
   end
 end
