5acbc684df531700de21a47a89fe4d0b7eac1b75
Abhishek Juneja <abhishek.juneja145@gmail.com>
2020-04-04 20:25:13 +0200

    updating the model annotations

--- spec/models/event_commit_sync_spec.rb
+++ spec/models/event_commit_sync_spec.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: event_commit_syncs
+#
+#  id               :bigint           not null, primary key
+#  response_payload :jsonb            not null
+#  status           :integer          not null
+#  sync_timestamp   :datetime
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  event_commit_id  :bigint           not null
+#
+# Indexes
+#
+#  index_event_commit_syncs_on_event_commit_id  (event_commit_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (event_commit_id => event_commits.id)
+#
 require 'rails_helper'
 
 RSpec.describe(EventCommitSync, type: :model) do
