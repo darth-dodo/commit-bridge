0eb627eea6b1f14d7e9aae594af9c228e515fd92
Abhishek Juneja <abhishek.juneja145@gmail.com>
2020-04-04 23:48:52 +0200

    using rack cors to restrict CO access

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,15 @@ module CommitBridge
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.middleware.insert_before(0, Rack::Cors) do
+      allow do
+        origins ENV['CORS_ORIGINS'].split(',').map(&:strip)
+        resource '*',
+                 headers: :any,
+                 methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+
     # Middleware for ActiveAdmin
     config.middleware.use(Rack::MethodOverride)
     config.middleware.use(ActionDispatch::Flash)
