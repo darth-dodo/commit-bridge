c770247d165b1920963cd8e206329f75991e3f3e
Abhishek Juneja <abhishek.juneja145@gmail.com>
2020-04-05 22:15:28 +0200

    adding execution level test cases for commit parser service

--- spec/services/commit_parser_spec.rb
+++ spec/services/commit_parser_spec.rb
@@ -70,7 +70,7 @@ RSpec.describe(CommitParser) do
       expect(Commit.exists?(sha: commit_sha)).to(be(true))
     end
 
-    it "should check ticket objects are created from the commit" do
+    it "should check ticket and project objects are created from the commit" do
       expect(Project.all.size).to(eq(0))
       expect(Ticket.all.size).to(eq(0))
 
@@ -85,16 +85,65 @@ RSpec.describe(CommitParser) do
       expect(Project.all.size).to(eq(commit.tickets.pluck('project_id').uniq.size))
     end
 
-    it "should check the commit is attached to the required tickets" do
+    it "should check new tickets and projects are *NOT* created for repeat occurrences" do
+      expect(Project.count).to(eq(0))
+      expect(Ticket.count).to(eq(0))
+
+      execute_service(ideal_service_payload)
+
+      old_projects_count = Project.count
+      old_tickets_count = Ticket.count
+
+      new_event = create(:push_request_event)
+      ideal_service_payload[:event] = new_event
+      ideal_service_payload["sha"] = Faker::Crypto.sha256
+      execute_service(ideal_service_payload)
+
+      new_projects_count = Project.count
+      new_tickets_count = Ticket.count
+
+      expect(new_projects_count).to(eq(old_projects_count))
+      expect(new_tickets_count).to(eq(old_tickets_count))
     end
 
-    it "should check commit is attached to the event" do
+    it "should check commit is attached to the event through event commit" do
+      expect(EventCommit.count).to(eq(0))
+      expect(new_event.commits.size).to(eq(0))
+
+      service_instance = execute_service(ideal_service_payload)
+      response_hashie = get_hashie(service_instance.service_response_data)
+
+      commit_sha = response_hashie.commit.sha
+      commit = Commit.find_by(sha: commit_sha)
+
+      new_event.reload
+      expect(new_event.commits.size).to(eq(1))
+      expect(EventCommit.count).to(eq(1))
+
+      event_commit = EventCommit.first
+      expect(event_commit.event_id).to(be(new_event.id))
+      expect(event_commit.commit_id).to(be(commit.id))
     end
 
     it "should check whether release is attached to a commit due to release event" do
+      release_event = create(:release)
+      ideal_service_payload[:event] = release_event.event_id
+      service_instance = execute_service(ideal_service_payload)
+      response_hashie = get_hashie(service_instance.service_response_data)
+
+      commit_sha = response_hashie.commit.sha
+      commit = Commit.find_by(sha: commit_sha)
+
+      expect(commit.release_id).to(eq(release_event.id))
     end
 
     it "should validate service response data keys" do
+      service_instance = execute_service(ideal_service_payload)
+      response_hashie = get_hashie(service_instance.service_response_data)
+      top_level_keys = response_hashie.keys
+      commit_level_keys = response_hashie.commit.keys
+      expect(top_level_keys).to(eq(["commit", "tickets"]))
+      expect(commit_level_keys).to(eq(["message", "sha", "linked_tickets_count"]))
     end
   end
 end
